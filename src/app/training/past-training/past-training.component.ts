23ed943ec018681eff72f8fba2f2b694d96859fb
alexandreguz <guzalexandre@gmail.com>
2024-12-09 12:01:50 +0200

    finished mat-data-table

--- src/app/training/past-training/past-training.component.ts
+++ src/app/training/past-training/past-training.component.ts
@@ -1,25 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { DatePipe, DecimalPipe } from '@angular/common';
 import { Exercise } from '../exercise.model';
 import { TrainingService } from '../training.service';
+import { MatSort, Sort, MatSortModule } from '@angular/material/sort';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
 
 @Component({
   selector: 'app-past-training',
   standalone: true,
-  imports: [MatTableModule, DatePipe, DecimalPipe],
+  imports: [MatTableModule, DatePipe, DecimalPipe, MatSortModule, MatFormFieldModule, MatInput, MatPaginator],
   templateUrl: './past-training.component.html',
   styleUrl: './past-training.component.css'
 })
-export class PastTrainingComponent  {
+export class PastTrainingComponent implements AfterViewInit {
   displayedColumns = ['date', 'name', 'calories', 'duration', 'state'];
   dataSource = new MatTableDataSource<Exercise>();
+
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(private traningService: TrainingService){}  
   
   ngOnInit(): void {
     this.dataSource.data = this.traningService.getCompletedOrCancelledExercises();
-
   }
 
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator
+  }
+
+
+  doFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase()
+  }
 }
